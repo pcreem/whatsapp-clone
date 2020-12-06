680f4a73cb614072744ce369e66564d2206d3f26
pcreem <creempepper@gmail.com>
2020-12-07 01:14:12 +0800

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA14P3VPyL9pogP9IDhY3odiorI53YvB1w",
-    authDomain: "whatsapp-clone-f9d56.firebaseapp.com",
-    databaseURL: "https://whatsapp-clone-f9d56.firebaseio.com",
-    projectId: "whatsapp-clone-f9d56",
-    storageBucket: "whatsapp-clone-f9d56.appspot.com",
-    messagingSenderId: "125667769910",
-    appId: "1:125667769910:web:9daf32d9ee2d7038366297"
-  };
+  apiKey: "AIzaSyBeTtjvg1Qvvg_CROCU8xh1ZDSnmkhwagY",
+  authDomain: "whatsapp-clone-d26b7.firebaseapp.com",
+  projectId: "whatsapp-clone-d26b7",
+  storageBucket: "whatsapp-clone-d26b7.appspot.com",
+  messagingSenderId: "274338046287",
+  appId: "1:274338046287:web:6990407799b9ac1e32253a"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
